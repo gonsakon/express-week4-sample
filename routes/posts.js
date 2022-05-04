e97aeb85f61cc2356d6c1a75b26945f1bf66f972
洧杰 廖 <gonsakon@gmail.com>
2022-05-04 18:36:19 +0800

    新增自定義錯誤設計

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const appError = require("../service/appError");
 const Post = require("../models/postsModel");
 const User = require("../models/usersModel");
+
 router.get('/', async function(req, res, next) {
   const timeSort = req.query.timeSort == "asc" ? "createdAt":"-createdAt"
   const q = req.query.q !== undefined ? {"content": new RegExp(req.query.q)} : {};
@@ -17,7 +19,9 @@ router.get('/', async function(req, res, next) {
 
 router.post('/', async function(req, res, next) {
     // query params
-    // console.log(req.body)
+    if(req.body.content == undefined){
+      return next(appError(400,"你沒有填寫 content 資料",next))
+    }
     const newPost = await Post.create(req.body);
     // res.send('<h1>1234</h1>');
     res.status(200).json({
