62e8875db9c352fa0c7dbefeedbb5c8ae5cabcf3
洧杰 廖 <gonsakon@gmail.com>
2022-05-05 16:32:27 +0800

    新增 JWT 登入與註冊方式

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,67 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const appError = require('../service/appError'); 
+const jwt = require('jsonwebtoken');
+const handleErrorAsync = require('../service/handleErrorAsync');
+const validator = require('validator');
+const User = require('../models/usersModel');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.status(200).json({
-    "name":"洧杰"
-  })
-});
-router.get('/login', function(req, res, next) {
-  res.status(200).json({
-    "name":"login"
-  })
-});
+const generateSendJWT= (user,statusCode,res)=>{
+  // 產生 JWT token
+  const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{
+    expiresIn: process.env.JWT_EXPIRES_DAY
+  });
+  user.password = undefined;
+  res.status(statusCode).json({
+    status: 'success',
+    user:{
+      token,
+      name: user.name
+    }
+  });
+}
+
+router.post('/sign_up', handleErrorAsync(async(req, res, next) =>{
+  let { email, password,confirmPassword,name } = req.body;
+  // 內容不可為空
+  if(!email||!password||!confirmPassword||!name){
+    return next(appError("400","欄位未填寫正確！",next));
+  }
+  // 密碼正確
+  if(password!==confirmPassword){
+    return next(appError("400","密碼不一致！",next));
+  }
+  // 密碼 8 碼以上
+  if(!validator.isLength(password,{min:8})){
+    return next(appError("400","密碼字數低於 8 碼",next));
+  }
+  // 是否為 Email
+  if(!validator.isEmail(email)){
+    return next(appError("400","Email 格式不正確",next));
+  }
+  
+  // 加密密碼
+  password = await bcrypt.hash(req.body.password,12);
+  const newUser = await User.create({
+    email,
+    password,
+    name
+  });
+  generateSendJWT(newUser,201,res);
+}))
+
+router.post('/sign_in',handleErrorAsync(async(req,res,next)=>{
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(appError( 400,'帳號密碼不可為空',next));
+  }
+  const user = await User.findOne({ email }).select('+password');
+  const auth = await bcrypt.compare(password, user.password);
+  if(!auth){
+    return next(appError(400,'您的密碼不正確',next));
+  }
+  generateSendJWT(user,200,res);
+}))
 
 module.exports = router;
