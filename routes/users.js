0715df9f51c7c6d0c07dd63610c5c21986a34c96
洧杰 廖 <gonsakon@gmail.com>
2022-05-05 16:36:43 +0800

    1.驗證是否已登入的 middleware 2.增加取得個人資料

--- routes/users.js
+++ routes/users.js
@@ -63,5 +63,41 @@ router.post('/sign_in',handleErrorAsync(async(req,res,next)=>{
   }
   generateSendJWT(user,200,res);
 }))
+const isAuth = handleErrorAsync(async (req, res, next) => {
+  // 確認 token 是否存在
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
 
+  if (!token) {
+    return next(appError(401,'你尚未登入！',next));
+  }
+
+  // 驗證 token 正確性
+  const decoded = await new Promise((resolve,reject)=>{
+    jwt.verify(token,process.env.JWT_SECRET,(err,payload)=>{
+      if(err){
+        reject(err)
+      }else{
+        resolve(payload)
+      }
+    })
+  })
+  const currentUser = await User.findById(decoded.id);
+
+  req.user = currentUser;
+  next();
+});
+
+router.get('/profile/',isAuth, handleErrorAsync(async(req, res, next) =>{
+
+  res.status(200).json({
+    status: 'success',
+    user: req.user
+  });
+}))
 module.exports = router;
