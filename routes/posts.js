a64c7d7e50aad774cc15b344426297470613b0aa
洧杰 廖 <gonsakon@gmail.com>
2022-05-05 16:52:15 +0800

    新增貼文路由，加上身份驗證

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const appError = require("../service/appError");
 const handleErrorAsync = require("../service/handleErrorAsync");
 const Post = require("../models/postsModel");
 const User = require("../models/usersModel");
-
+const {isAuth,generateSendJWT} = require('../service/auth');
 router.get('/', async function(req, res, next) {
   const timeSort = req.query.timeSort == "asc" ? "createdAt":"-createdAt"
   const q = req.query.q !== undefined ? {"content": new RegExp(req.query.q)} : {};
@@ -18,16 +18,19 @@ router.get('/', async function(req, res, next) {
   })
 });
 
-router.post('/', handleErrorAsync(async function(req, res, next) {
-    if(req.body.content == undefined){
-      return next(appError(400,"你沒有填寫 content 資料",next))
-    }
-      const newPost = await Post.create(req.body);
-      // res.send('<h1>1234</h1>');
-      res.status(200).json({
-        status:"success",
-        post: newPost
-      })
-  }));
+router.post('/',isAuth, handleErrorAsync(async function(req, res, next) {
+  const { content } = req.body;
+  if(content == undefined){
+    return next(appError(400,"你沒有填寫 content 資料",next))
+  }
+  const newPost = await Post.create({
+    user: req.user.id,
+    content
+  });
+  res.status(200).json({
+    status:"success",
+    post: newPost
+  })
+}));
 
 module.exports = router;
