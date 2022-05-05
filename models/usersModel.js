e242b2b91fb136d4124b205b8470dccad046d8eb
洧杰 廖 <gonsakon@gmail.com>
2022-05-05 16:08:28 +0800

    增加user model

--- models/usersModel.js
+++ models/usersModel.js
@@ -12,6 +12,21 @@ const userSchema = new mongoose.Schema({
       select: false
     },
     photo: String,
+    sex:{
+      type: String,
+      enum:["male","female"]
+    },
+    password:{
+      type: String,
+      required: [true,'請輸入密碼'],
+      minlength: 8,
+      select: false
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      select: false
+    }
   });
 // User
 const User = mongoose.model('user', userSchema);
