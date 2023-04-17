45be1e995ff2493af653ef160f5bac42789179ed
gonsakon <gonsakon@gmail.com>
2023-04-17 12:23:31 +0800

    add storage

--- routes/upload.js
+++ routes/upload.js
@@ -5,6 +5,11 @@ const handleErrorAsync = require("../service/handleErrorAsync");
 const sizeOf = require('image-size');
 const upload = require('../service/image');
 const { ImgurClient } = require('imgur');
+
+const { v4: uuidv4 } = require('uuid');
+const firebaseAdmin = require('../service/firebase');
+const bucket = firebaseAdmin.storage().bucket();
+
 const {isAuth,generateSendJWT} = require('../service/auth');
 router.post('/', isAuth,upload,handleErrorAsync(async (req, res, next)=> {
     if(!req.files.length) {
@@ -30,4 +35,45 @@ router.post('/', isAuth,upload,handleErrorAsync(async (req, res, next)=> {
     })
 }));
 
+router.post('/file',upload,handleErrorAsync(async (req, res, next)=> {
+
+  if(!req.files.length) {
+    return next(appError(400,"尚未上傳檔案",next));
+  }
+  // 取得上傳的檔案資訊列表裡面的第一個檔案
+  const file = req.files[0];
+  // 基於檔案的原始名稱建立一個 blob 物件
+  const blob = bucket.file(`images/${uuidv4()}.${file.originalname.split('.').pop()}`);
+  // 建立一個可以寫入 blob 的物件
+  const blobStream = blob.createWriteStream()
+
+  // 監聽上傳狀態，當上傳完成時，會觸發 finish 事件
+  blobStream.on('finish', () => {
+    // 設定檔案的存取權限
+    const config = {
+      action: 'read', // 權限
+      expires: '12-31-2500', // 網址的有效期限
+    };
+    // 取得檔案的網址
+    blob.getSignedUrl(config, (err, fileUrl) => {
+      res.send({
+        fileUrl
+      });
+    });
+  });
+
+  // 如果上傳過程中發生錯誤，會觸發 error 事件
+  blobStream.on('error', (err) => {
+    res.status(500).send('上傳失敗');
+  });
+
+  // 將檔案的 buffer 寫入 blobStream
+  blobStream.end(file.buffer);
+
+
+  // res.status(200).json({
+  //     status:"success",
+  //     imgUrl: response.data.link
+  // })
+}));
 module.exports = router;
